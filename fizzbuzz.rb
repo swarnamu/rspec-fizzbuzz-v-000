daad5e08a388905263fc7492a05654cd107781e6
Swarna Muralidharan <swarna.muralic@gmail.com>
2017-10-20 20:28:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if (int % 3 == 0 && int % 5) # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    "FizzBuzz" # Go fizz
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 == 0
-    "FizzBuzz"
+    "Fizz"
   else
     nil
   end
